11a670410f2d223728878f5a6430de76eb38daa2
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-04-17 17:54:30 -0600

    envolviendo la aplicación con el provider de autenticación

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { AuthProvider } from '@/context/authProvider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <AuthProvider>
+      <Component {...pageProps} />
+
+    </AuthProvider>
+  )
 }
